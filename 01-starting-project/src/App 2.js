7d32219d5237599452df10ee98711a0695b6aebf
“sukyung6999” <sukyung6999@gmail.com>
2023-12-21 22:48:27 +0900

    add: [섹션 9] 리듀서를 사용하여 부작용 처리 & 컨텍스트 API 사용하기
    - useReducer 초반

---
+++ 01-starting-project/src/App 2.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const isLoggedInInformation = localStorage.getItem('login');
+
+    if (isLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, [])
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('login', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('login');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
